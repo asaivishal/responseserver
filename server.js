f217212512612347fe050c93513c349706993602
vishal.asai <vishal.asai@leisurepools.com.au>
2015-01-30 12:40:42 +1000

    Cakephp requires hostname not to be IP address, used body-parser middleware

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-var express   = require('express');
-var app       = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.post('/', function(req, res){
-	console.log(req.query);
+app.post('/', function(req, res) {
+    console.log(req.body);
+    res.json(req.body);
 });
-
 
 // bootstrap the server.
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(3000, 'leisureresserver.dev', function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log("Yo, you fuck - this app listening at http://%s:%s", host, port);
+    console.log("Yo, you fuck - this app listening at http://%s:%s", host, port);
 });
+
